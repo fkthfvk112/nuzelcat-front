c503b2bfa67f27acea428b1a2f347c8b14c4aa84
Jung jinseong <wjdwl545@naver.com>
2025-09-01 00:20:48 +0900

    add user header

--- app/(utils)/serverFetch.ts
+++ app/(utils)/serverFetch.ts
@@ -1,24 +1,46 @@
-"use server"
+"use server";
 
-export default async function serverFetch({url, queryParams, option}:{url:string, queryParams?:object, option?:RequestInit}):Promise<any>{
-  const queryString = queryParams ? `?${objectToQueryString(queryParams)}` : '';
+import { headers } from "next/headers";
+
+export default async function serverFetch({
+  url,
+  queryParams,
+  option
+}: {
+  url: string;
+  queryParams?: object;
+  option?: RequestInit;
+}): Promise<any> {
+  const queryString = queryParams ? `?${objectToQueryString(queryParams)}` : "";
   const fullUrl = `${process.env.NEXT_PUBLIC_API_URL}${url}${queryString}`;
+
+  // ✅ 비동기 호출
+  const incomingHeaders = await headers();
+
+  const forwardHeaders: HeadersInit = {
+    ...(option?.headers || {}),
+    "x-forwarded-for": incomingHeaders.get("x-forwarded-for") || "",
+    "user-agent": incomingHeaders.get("user-agent") || "",
+    "accept-language": incomingHeaders.get("accept-language") || ""
+  };
 
   const res = await fetch(fullUrl, {
     ...option,
-    cache: "no-store",
+    headers: forwardHeaders,
+    cache: "no-store"
   });
-  
-  if (!res.ok) {
-      const result = await res.json();
-      console.log("결과", result);
-      throw new Error(result?.message || '알 수 없는 오류가 발생했습니다.');
-    }
-    return res.json();
- }
 
-const objectToQueryString = (queryParam:Object)=>{
-    return Object.entries(queryParam)
-            .map(([key, value]) => `${encodeURIComponent(key)}=${encodeURIComponent(value)}`)
-            .join('&');
+  if (!res.ok) {
+    const result = await res.json();
+    console.log("결과", result);
+    throw new Error(result?.message || "알 수 없는 오류가 발생했습니다.");
+  }
+
+  return res.json();
 }
+
+const objectToQueryString = (queryParam: Object) => {
+  return Object.entries(queryParam)
+    .map(([key, value]) => `${encodeURIComponent(key)}=${encodeURIComponent(value)}`)
+    .join("&");
+};
