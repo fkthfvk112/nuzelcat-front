fcd5139dca2049e13c84d80f9d04a290c4f415a5
Jung jinseong <wjdwl545@naver.com>
2025-08-31 23:49:25 +0900

    cache -> no cache

--- app/(utils)/serverFetch.ts
+++ app/(utils)/serverFetch.ts
@@ -1,11 +1,15 @@
 "use server"
 
 export default async function serverFetch({url, queryParams, option}:{url:string, queryParams?:object, option?:RequestInit}):Promise<any>{
-    const queryString = queryParams ? `?${objectToQueryString(queryParams)}` : '';
-    const fullUrl = `${process.env.NEXT_PUBLIC_API_URL}${url}${queryString}`;
+  const queryString = queryParams ? `?${objectToQueryString(queryParams)}` : '';
+  const fullUrl = `${process.env.NEXT_PUBLIC_API_URL}${url}${queryString}`;
 
-    const res = await fetch(`${fullUrl}`, option);
-    if (!res.ok) {
+  const res = await fetch(fullUrl, {
+    ...option,
+    cache: "no-store",
+  });
+  
+  if (!res.ok) {
       const result = await res.json();
       console.log("결과", result);
       throw new Error(result?.message || '알 수 없는 오류가 발생했습니다.');
